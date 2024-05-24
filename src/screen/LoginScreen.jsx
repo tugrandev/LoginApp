abfc55fe65c6251df1852ac5039fa2f9e0081d48
Tugran Yayla <tugrandev@gmail.com>
2024-05-25 02:43:58 +0300

    Day 2 of developing is done.

--- src/screen/LoginScreen.jsx
+++ src/screen/LoginScreen.jsx
@@ -1,6 +1,8 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import LinearGradient from 'react-native-linear-gradient';
 
 const LoginScreen = () => {
   return (
@@ -17,9 +19,23 @@ const LoginScreen = () => {
       <View>
         <Text style= {styles.signContainer}>Hesabınıza giriş yapın</Text>
       </View>
-      <FontAwesome name= {"user"} size= {30} color={"black"}>
-
-      </FontAwesome>
+      <View style= {styles.inputContainer}>
+        <FontAwesome name= {"user"} size= {24} color={"#9A9A9A"} style= {styles.inputIcon}/>
+        <TextInput style= {styles.TextInput} placeholder='Telefon No'/>
+      </View>
+      <View style= {styles.inputContainer}>
+        <Fontisto name= {"locked"} size= {24} color={"#9A9A9A"} style= {styles.inputIcon}/>
+        <TextInput style= {styles.TextInput} placeholder='Şifre' secureTextEntry/>
+      </View>
+      <Text style= {styles.forgotPass}>Şifremi unuttum</Text>
+      <View style= {styles.signInButtonContainer}>
+        <Text style= {styles.signIn}>Giriş yap</Text>
+        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+          <Text style={styles.buttonText}>
+            Sign in with Facebook
+          </Text>
+        </LinearGradient>
+      </View>
     </View>
   )
 }
@@ -37,14 +53,15 @@ const styles = StyleSheet.create({
 
   topImage: {
     width: "100%",
-    height: 119,
+    height: 125,
   },
 
   merhabaContainer: {},
 
   merhabaText: {
+    marginVertical: 20,
     textAlign: "center",
-    fontSize: 70,
+    fontSize: 70, 
     fontWeight: "500",
     color: "#262626"
   },
@@ -53,6 +70,50 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     color: "#262626"
+  },
+
+  inputContainer: {
+    backgroundColor: "#FFFFFF", 
+    flexDirection: "row",
+    borderRadius: 40,
+    marginHorizontal: 40,
+    marginVertical: 65,
+    marginBottom: -30,
+    marginLeft: 45,
+    elevation: 5,
+    alignItems: "center",
+    borderWidth: 1, 
+    borderColor: "#9A9A9A",
+    width: 300,
+    height: 50, 
+    paddingHorizontal: 10, 
+    
+  },
+  inputIcon: {
+    marginLeft: 9, 
+    marginRight: 5,
+  },
+  
+  TextInput: {
+    flex: 1,
+    paddingHorizontal: 10,
+    height: "100%", 
+  },
+
+  forgotPass: {
+    color: "#BEBEBE",
+    marginLeft: 230,
+    marginTop: 45,
+    width: "85%",
+    fontSize: 15,
+  },
+
+  signInButtonContainer: {},
+
+  signIn: {
+    color: "#262626",
+    fontSize: 25,
+    fontWeight: "bold",
   }
 
 })
